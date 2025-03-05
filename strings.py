0e9b6ae4f1a329ee7a2541eafcd283c960318f9a
Ofuzor chukwuemeke <uniben2018@gmail.com>
2025-03-05 08:32:28 +0100

    strings

---
+++ strings.py
@@ -0,0 +1,9 @@
+# Strings are one of the fundamental python Data Type
+
+name = "Ofuzor Chukwuemeke"
+phone_number = "08012345678"
+address = "No 1, Python Street, Lagos"
+string3 = "Python is a programming language \"Python\""
+
+print(string3);
+print(len(address))
